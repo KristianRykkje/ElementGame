7eb6945e7fa116aa9ea44b4fee36335a55d74fe0
KristianRykkje <kriryk@gmail.com>
2021-06-15 22:37:41 +0200

    at this point I don't know what I'm doing

--- webpack/base.js
+++ webpack/base.js
@@ -6,17 +6,11 @@ module.exports = {
   devtool: "eval-source-map",
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   use: {
-      //     loader: "babel-loader",
-      //   },
-      // },
-      // {
-      //   test: [/\.vert$/, /\.frag$/],
-      //   use: "raw-loader",
-      // },
+      // { test: /\.js$/, use: ["babel-loader"] },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: "raw-loader",
+      },
       {
         test: /\.(gif|png|jpe?g|svg|xml)$/i,
         use: "file-loader",
